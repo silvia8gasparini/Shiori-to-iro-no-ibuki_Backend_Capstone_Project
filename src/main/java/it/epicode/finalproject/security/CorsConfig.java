59971ed0f900b3c4f60f1b44c24835bd02989ed8
Silvia <silvia8gasparini@gmail.com>
2025-07-09 18:25:14 +0200

    add 3 more colors

--- src/main/java/it/epicode/finalproject/security/CorsConfig.java
+++ src/main/java/it/epicode/finalproject/security/CorsConfig.java
@@ -13,8 +13,8 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // tutte le rotte
-                        .allowedOrigins("http://localhost:5173") // il tuo front-end
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
