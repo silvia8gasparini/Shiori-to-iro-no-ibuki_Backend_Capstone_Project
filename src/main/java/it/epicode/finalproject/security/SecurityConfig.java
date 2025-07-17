0341794a36f7b5ce62971a6a7dc8e4f99ca84ee7
Silvia <silvia8gasparini@gmail.com>
2025-07-17 17:06:33 +0200

    Update SecurityConfig.java

--- src/main/java/it/epicode/finalproject/security/SecurityConfig.java
+++ src/main/java/it/epicode/finalproject/security/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -52,4 +57,23 @@ public class SecurityConfig {
     public JwtFilter jwtFilter(JwtTool jwtTool) {
         return new JwtFilter(jwtTool);
     }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://shiori-to-iro-no-ibuki-frontend-capstone-project-pgaiicn43.vercel.app",
+                "https://desperate-hillary-silviagasparini-906caa83.koyeb.app"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 }
