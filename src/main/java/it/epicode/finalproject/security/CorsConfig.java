ac3ec54ad5a6169d59e1e0b44db2681f87ad1bb0
Silvia <silvia8gasparini@gmail.com>
2025-07-17 15:31:11 +0200

    Update CorsConfig.java

--- src/main/java/it/epicode/finalproject/security/CorsConfig.java
+++ src/main/java/it/epicode/finalproject/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://shiori-to-iro-no-ibuki-frontend-capstone-project-pgaiicn43.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
